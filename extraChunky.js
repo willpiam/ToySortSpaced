82b75b877b1fa4c7c1f91b605196bd7d9675c9a3
William Doyle <mcbidebilly@gmail.com>
2021-07-13 11:04:40 -0400

    sort chunks

--- extraChunky.js
+++ extraChunky.js
@@ -20,8 +20,8 @@ function randomNumber() {
 	return Math.floor(Math.random() * 100);
 }
 
-
 const arr = [];
+
 for (let i = 0; i < 15; i++)
 	arr.push({ color: randomColor(), number: randomNumber() });
 
@@ -29,14 +29,12 @@ for (let i = 0; i < 15; i++)
 for (let i = 0; i < 10; i++)
 	arr.push({ color: "red", number: randomNumber() });
 
-
 console.log(`ORIGINAL ARRAY`);
 console.log(arr);
 
 console.log(`SORTED BY NUMBER ARRAY`);
 arr.sort((a, b) => a.number - b.number);
 console.log(arr);
-
 
 function swapInArr(_arr, i, k) {
 	[_arr[i], _arr[k]] = [_arr[k], _arr[i]];
@@ -55,8 +53,6 @@ for (let i = 0; i < arr.length - 1; i++)
 
 console.log(arr);
 
-
-
 console.log(`Now go from back to front`);
 
 for (let i = arr.length - 1; i > 0; i--)
@@ -68,5 +64,37 @@ for (let i = arr.length - 1; i > 0; i--)
 			break;
 		}
 
+console.log(arr);
+
+console.log(`Now find any remining clumps and sort them by number`);
+
+for (let i = 0; i < arr.length - 1; i++) {
+
+	if (arr[i].color !== arr[i + 1].color)
+		continue;
+
+	const clump = [arr[i]];
+
+	for (let k = i + 1; k < arr.length; k++) {
+		if (arr[k].color === arr[i].color) {
+			clump.push(arr[k]);
+			continue;
+		}
+		break;
+	}
+
+	// sort clump and replace it in the array
+	console.log(`pre sort`);
+	console.log(clump);
+	clump.sort((a, b) => a.number - b.number);
+	console.log(`post sort`);
+	console.log(clump);
+
+	// splice out old clump and replace it with sorted clump
+	arr.splice(i, i + clump.length, ...clump);
+
+	// jump i by length of clump
+	i += clump.length - 1;
+}
 
 console.log(arr);
